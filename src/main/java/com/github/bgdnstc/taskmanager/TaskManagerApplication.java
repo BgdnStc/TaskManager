edd741393845b611f96d246628458a7e7798511d
Bogdan Stoica <bogdanstoica10@gmail.com>
2025-05-09 19:00:45 +0300

    tests, postman scripts

--- src/main/java/com/github/bgdnstc/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/github/bgdnstc/taskmanager/TaskManagerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskManagerApplication {
 
+    // Java Spring application run
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
     }
